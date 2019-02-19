25e195ea0496977121166c86453cda178f04e9c9
amannmaa2014 <amannmaa2014@gmail.com>
2019-02-19 08:46:41 +0300

    carMarket

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib import messages
 from .models import Car
+from .forms import CarForm
 
 def car_list(request):
 	cars = Car.objects.all()
@@ -18,15 +20,42 @@ def car_detail(request, car_id):
 
 
 def car_create(request):
-	#Complete Me
-	return render(...)
+	form = CarForm()
+	if request.method =="POST":
+		form = CarForm(request.POST, request.FILES or None) #request.FILES if we are getting an image files or none"it is optional"
+		if form.is_valid():
+			form.save()
+			messages.success(request, 'successfuly created!')
+			return redirect('car-list')
+	context = {
+	 'form':form,
+	}
+	return render(request, 'create.html',context)
 
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
+	car = Car.objects.get(id=car_id)
+	form = CarForm(instance= car)
+	if request.method =="POST":
+		form = CarForm(request.POST, request.FILES or None, instance= car)
+		if form.is_valid():
+			form.save()
+			return redirect('car-list')
+			#return redirect(car)
+			#this redirect to detail page and it is only work if there is car.get_absolute_url
+	context = {
+	'form':form,
+	'car':car,
+	}
+	return render(request, 'update.html',context)
 
 
 def car_delete(request, car_id):
 	#Complete Me
-	return render(...)
+	Car.objects.get(id=car_id).delete()
+	messages.success(request, 'successfuly deleted!')
+	return redirect('car-list')
+
+
+
+
